c03376b9414ad1e126082591447528b61041a5eb
Christine VanDehey <chrisdalvan@gmail.com>
2022-02-26 16:37:41 -0800

    Software Development Lifecycle

---
+++ sldc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const Analysis = "This is the second step, in which you can think of software architecture as the blueprint for the development team. A prototype version lets the development team and the stakeholders visualize the overall look and feel of the software"
+const Design = "This is the third step, in which coders who work on bringing the concept into reality. Once the code is ready, the development team shares it with the testing team for review."
+const Implementation = "This is the fourth step, in which the quality assurance team works with the development team to test and report bugs and inconsistencies in the written code."
+const Testing = "This is the fifth step, in which the written code/ software goes into the production environment phase after it is developed, tested, fixed, retested and validated. If the end-users experience any issue with the software, it moves back to the softwre development team for reconsideration and fixing."
+const Maintain = "This is the sixth step, in which maintenance simply implies that softwre requires updates from time to time, which needs to be taken care of to uphold the software's value proposition. Including corrective maintenance to address any bugs and improve performance and perfective maintenance keeping up with current expectations to stay relevant and valuable."
